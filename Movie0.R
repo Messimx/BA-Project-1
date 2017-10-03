c2b9a6f7cf59c10ac82a6feeaa2f314f81c24ad0
Xi Mei <xm518@nyu.edu>
2017-10-03 14:54:51 -0400

    Update Movie0.R

--- Movie0.R
+++ Movie0.R
@@ -2,13 +2,11 @@ library(corrplot)
 install.packages("corrgram")
 library(corrgram)
 
-dataframe0<-read.csv("https://raw.githubusercontent.com/Messimx/BA-Project-1/master/Final%20dataset%20(1).csv", header=TRUE, stringsAsFactors = FALSE)
+dataframe0<-read.csv("https://raw.githubusercontent.com/Messimx/BA-Project-1/master/Final%20Countries%20Dataset.csv", header=TRUE, stringsAsFactors = FALSE)
 datatype<-as.data.frame(sapply(dataframe0,class))
 
 #Filter dataset
 dataframe1<-dataframe0[-1]
-dataframe1$Net.Trade..Exports...Imports..in....<-NULL
-
 
 #Descriptive analysis of Xs
 #mode function
@@ -49,7 +47,7 @@ cordf1
 
 #Normalization
 norm.fun<-function(x){
-  norm.max<-max(x,na.rm=TRUE)
+  norm.max<-max(abs(x),na.rm=TRUE)
   norm.result<-x/norm.max*100
 }
 dataframe2<-as.data.frame(sapply(dataframe1,norm.fun))
@@ -58,3 +56,26 @@ summary.dataframe2<-sapply(dataframe2,descriptive.summary)
 cordf2<-as.data.frame(cor(dataframe2,use = "complete.obs"))
 corrgram(cordf2, order = TRUE, upper.panel = panel.cor,main = "cordf2",col.regions = colorRampPalette(c(
   "darkred", "blue")))
+
+#Assigning weights
+weights_var<-dataframe2[1,]
+
+summary_eco<-as.data.frame(summary.dataframe2["cv",c("GDP","Net.Imports","Net.Exports","Foreign.Direct.Investment")])
+cv_eco<-sum(summary_eco)
+weights_var$GDP<-(0.4*as.numeric(summary.dataframe2["cv","GDP"]))/cv_eco
+weights_var$Net.Imports<-(0.4*as.numeric(summary.dataframe2["cv","Net.Imports"]))/cv_eco
+weights_var$Net.Exports<-(0.4*as.numeric(summary.dataframe2["cv","Net.Exports"]))/cv_eco
+weights_var$Foreign.Direct.Investment<-(0.4*as.numeric(summary.dataframe2["cv","Foreign.Direct.Investment"]))/cv_eco
+
+summary_env<-as.data.frame(summary.dataframe2["cv",c("CO2.emissions...population","Renewable.Energy.Consumption")])
+cv_env<-sum(summary_env)
+weights_var$CO2.emissions...population<-(0.2*as.numeric(summary.dataframe2["cv","CO2.emissions...population"]))/cv_env
+weights_var$Renewable.Energy.Consumption<-(0.2*as.numeric(summary.dataframe2["cv","Renewable.Energy.Consumption"]))/cv_env
+
+weights_var$HDI<-0.2
+weights_var$Central.Government.Debt<-0.2
+weights_var[2:27,]<-weights_var
+
+#Calculate the index value
+dataframe2$Index<-rowSums(dataframe2*weights_var)
+dataframe2$Name<-dataframe0[1]
